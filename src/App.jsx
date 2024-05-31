8e2d3de5e228d29f800b1f25f924cf3cb715751d
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-31 08:59:15 +0000

    App.jsx file modified

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Javeria Rehman",
+  title: "Flutter and Front End Developer",
+  email: "jia.rehman122@gmail.com",
+  gitHub: "javeeriiaaa",
+  instagram: "javeeriiaaa",
+  linkedIn: "thejaveria",
+  //medium: "",
+  //twitter: "microsoft",
+  //youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
