3a5d143ca730ddb6d59e4f41f65b39b913422973
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-31 08:59:03 +0000

    About section changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student at Air University, Islamabad, with a passion for developing Mobile Apps and desiging creative user interfaces for Websites.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "App Development",
+  "Front End Development",
+  "Flutter",
+  "React Native",
+  //"Mobile user interfaces",
+  //"Graphic design",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "As a Flutter developer, I create innovative mobile apps, and as a front-end developer, I design intuitive and engaging websites. My work combines creativity and technical skill to deliver exceptional digital experiences.";
 
 const About = () => {
   return (
